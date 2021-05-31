9577550840b7161d8defd7838c418c057d18f4b3
Nausher Rao <SherRao15@gmail.com>
2021-05-31 17:39:34 -0400

    Added utility functions
    Deleted extra useless command and task file.
    Formatting updates.

--- commands/example.command.js
+++ commands/example.command.js
@@ -1,65 +1,62 @@
-const util = require('../util');
-const main = require('../index');
+const util = require("../util");
+const main = require("../index");
 const discord = main.discord;
 const logger = main.logger;
 
 /**
- * 
+ *
  * A command file that contains the JSON needed to POST the command
- * to the Discord command endpoint AND the code needed to handle 
+ * to the Discord command endpoint AND the code needed to handle
  * the command when it's triggered.
- * 
+ *
  * Reference -> https://discord.com/developers/docs/interactions/slash-commands#registering-a-command
- * 
+ *
  * @author Nausher Rao
- * 
+ *
  */
 module.exports = {
-
     // Data object that includes all the JSON to post to the Discord command endpoint.
-    "data": {
-        "name": "hello",
-        "description": "Henlo.",
-        "default_permission": false, //By default, nobody has permission if set to false
+    data: {
+        name: "hello",
+        description: "Henlo.",
+        default_permission: false, //By default, nobody has permission if set to false
 
-        "permissions": [ 
-
+        permissions: [
             {
-                "id": "ROLE_ID_1",
-                "type": 1,
-                "permission": true,
+                id: "ROLE_ID_1",
+                type: 1,
+                permission: true,
             },
 
             {
-                "id": "ROLE_ID_1",
-                "type": 1,
-                "permission": true,
-            }
-
+                id: "ROLE_ID_1",
+                type: 1,
+                permission: true,
+            },
         ],
 
-        "type": 2,
-        "options": [
-
+        type: 2,
+        options: [
             {
-                "name": "world",
-                "description": "Its world",
-                "type": 1,
-                "options": [],
-            }, 
-
-            {
-                "name": "nausher",
-                "description": "Its me",
-                "type": 1,
-                "options": [{
-                    "name": "rao",
-                    "description": "Its me x2.",
-                    "type": 1,
-                    "required": false,
-                }],
+                name: "world",
+                description: "Its world",
+                type: 1,
+                options: [],
             },
 
+            {
+                name: "nausher",
+                description: "Its me",
+                type: 1,
+                options: [
+                    {
+                        name: "rao",
+                        description: "Its me x2.",
+                        type: 1,
+                        required: false,
+                    },
+                ],
+            },
         ],
     },
 
@@ -67,7 +64,5 @@ module.exports = {
     execute: (interaction) => {
         logger.info("Someone used the example command!");
         util.sendMessage("Hello world!", interaction);
-
-    }
-
-}
+    },
+};
--- events/example.events.js
+++ events/example.events.js
@@ -1,22 +1,19 @@
-const util = require('../util');
-const main = require('../index');
+const util = require("../util");
+const main = require("../index");
 const discord = main.discord;
 const logger = main.logger;
 
 /**
- * 
+ *
  * Example command that prints a ready message when the bot turns on.
- * 
+ *
  */
 module.exports = {
-
     name: "ready",
-    
+
     once: true,
 
     execute: () => {
         logger.info(`Ready! Logged in as ${discord.user.tag}!`);
-
-    }
-
-}
+    },
+};
