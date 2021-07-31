bb140d0b01ef1f643847d14449dfcfdb49d10f42
Nausher Rao <SherRao15@gmail.com>
2021-07-31 13:26:39 -0400

    Fix a small issue with the config file location being wrong
    This commit also adds a script to run "npm start"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const config = require("../config.json");
+const config = require("./config.json");
 
 const Discord = require("discord.js");
 const discord = new Discord.Client();
