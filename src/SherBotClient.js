bc3d57863f4166fa29c80699a3e052afcf04cf89
Nausher Rao <SherRao15@gmail.com>
2022-03-11 23:25:51 -0500

    Setup first verseion of NPM module SherBot

---
+++ src/SherBotClient.js
@@ -0,0 +1,73 @@
+const { Client } = require("discord.js");
+
+const defaultOptions = {
+    commandsDirectory: "commands",
+    eventsDirectory: "events",
+    embedsDirectory: "embeds",
+    tasksDirectory: "tasks",
+};
+
+class SherBotClient {
+
+    /**
+     * Creates a new SherBotClient.
+     * 
+     * @param {ClientOptions} [clientOptions] Options for the regular Discord client.
+     * @param {SherBotOptions} [options] Options to be used for SherBot.
+     * 
+     */
+    constructor(clientOptions = {}, options = defaultOptions) {
+        this.client = new Client(clientOptions);
+        this.options = options;
+
+        Object.defineProperty(this, "token", { writable: true });
+        if (!browser && !this.token && "DISCORD_TOKEN" in process.env) {
+            /**
+             * Authorization token for the logged in bot.
+             * If present, this defaults to `process.env.DISCORD_TOKEN` when instantiating the client
+             * <warn>This should be kept private at all times.</warn>
+             * @type {?string}
+             */
+            this.token = process.env.DISCORD_TOKEN;
+
+        } else
+            this.token = null;
+    }
+
+    /**
+     * Logs the client in, establishing a websocket connection to Discord.
+     * 
+     * @param {string} [token=this.token] Token of the account to log in with
+     * @returns {Promise<string>} Token of the account used
+     * @example client.login('my token');
+     * 
+     */
+    async login(token = this.token) {
+        this.client.once("ready", () => {
+            initLogger();
+            setPresence();
+            registerCommands();
+            registerEvents();
+            registerTasks();
+            handleCommands();
+
+            logger.info("Bot loaded!");
+        });
+
+        return this.client.login(token);
+    }
+
+    #initLogger() { }
+
+    #setPresence() { }
+
+    #registerCommands() { }
+
+    #registerEvents() { }
+
+    #registerTasks() { }
+
+    #handleCommands() { }
+}
+
+module.exports = SherBotClient;
